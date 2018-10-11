62bf35a3115c0dc9c3f6819e85ecac1c9cdeaa70
chenhuang444 <chenhuang444@gmail.com>
2018-10-11 21:43:00 +0800

    Initial Commit

---
+++ InteropTest/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace InteropTest.Adapter
+{
+    [SuppressUnmanagedCodeSecurity]
+    public unsafe class MyAdapter
+    {
+        private const string DLLName = "VersionNumber";
+
+        private const string EntryPointName = "GetMyVersion";
+
+        [DllImport(DLLName, EntryPoint = EntryPointName, CharSet = CharSet.Ansi)]
+        internal static extern IntPtr GetMyVersionPtr();
+
+        [DllImport(DLLName, EntryPoint = EntryPointName, CharSet = CharSet.Ansi)]
+        internal static extern string GetMyVersionString();
+    }
+}
---
+++ InteropTest/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using InteropTest.Adapter;
+
+namespace InteropTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var ptr = MyAdapter.GetMyVersionPtr();
+            var convertedString = Marshal.PtrToStringAnsi(ptr);
+
+            // This crashes in x64 build
+            var directString = MyAdapter.GetMyVersionString();
+            
+            VersionTextBlock.Text = convertedString;
+        }
+    }
+}
